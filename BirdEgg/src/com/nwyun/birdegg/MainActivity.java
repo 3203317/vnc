6d5900cb37d20c19fa6dfdff8821d5f364f6622a
unknown <3203317@qq.com>
2015-10-09 09:59:09 +0800

    haha

--- BirdEgg/src/com/nwyun/birdegg/MainActivity.java
+++ BirdEgg/src/com/nwyun/birdegg/MainActivity.java
@@ -1,16 +1,19 @@
 package com.nwyun.birdegg;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
+/**
+ * 
+ * @author huangxin (3203317@qq.com)
+ * 
+ */
 public class MainActivity extends ActionBarActivity {
 
 	@Override
---
+++ BirdEgg/src/com/nwyun/birdegg/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.nwyun.birdegg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 
+ * @author huangxin (3203317@qq.com)
+ * 
+ */
+public class SplashActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		// TODO
+	}
+}
