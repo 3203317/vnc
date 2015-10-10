adbcb01776634f2d834a0612efd2bc89a1c124e0
unknown <3203317@qq.com>
2015-10-10 23:21:30 +0800

    haha

--- viewer/src/net/foreworld/test/Test.java
+++ viewer/src/net/foreworld/test/Test.java
@@ -1,7 +1,28 @@
 package net.foreworld.test;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Test {
+	private String ip = "192.168.6.128";
+	private int port = 5901;
+
+	private Socket sock;
+
 	public static void main(String[] args) {
-		System.out.println("viewer");
+
+		Test t = new Test();
+		try {
+			t.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void start() throws UnknownHostException, IOException {
+		sock = new Socket(ip, port);
+		System.out.println(sock.getInputStream());
 	}
 }
