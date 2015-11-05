4f53078505be2eaa208e79ecc7c72a13d0fb6aad
unknown <3203317@qq.com>
2015-11-05 12:33:42 +0800

    hhaa

--- vnc/com/nwyun/birdegg/client/Connector.java
+++ vnc/com/nwyun/birdegg/client/Connector.java
@@ -1,7 +1,13 @@
 package com.nwyun.birdegg.client;
 
-import com.glavsoft.viewer.RfbConnectionWorker;
-import com.nwyun.birdegg.client.swing.SwingRfbConnectionWorker;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 
 /**
  * 
@@ -11,13 +17,43 @@ import com.nwyun.birdegg.client.swing.SwingRfbConnectionWorker;
 public class Connector {
 	private RfbServer _server;
 
+	private JFrame _frame;
+	private JScrollPane _scroller;
+
 	public Connector(RfbServer server) {
 		_server = server;
 	}
 
 	public void connect() {
-		System.out.println(_server);
-		RfbConnectionWorker work = new SwingRfbConnectionWorker();
-		work.execute();
+		System.out.println("connect");
+		createContainer();
+	}
+
+	private void createContainer() {
+		Dimension dim = new Dimension();
+		dim.width = 1024;
+		dim.height = 768;
+
+		_scroller = new JScrollPane();
+
+		_frame = new JFrame();
+		_frame.setSize(dim);
+		_frame.setLayout(new BorderLayout(0, 0));
+
+		_frame.add(_scroller, BorderLayout.CENTER);
+
+		_frame.setVisible(true);
+		_frame.validate();
+		_frame.setTitle("vnc");
+
+		_frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent we) {
+				int ret = JOptionPane.showConfirmDialog(null, "确定关闭吗？",
+						"Attention", JOptionPane.OK_OPTION);
+				if (ret == JOptionPane.OK_OPTION)
+					System.exit(0);
+			}
+		});
 	}
 }
