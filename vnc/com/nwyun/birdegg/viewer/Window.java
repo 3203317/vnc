28ea692937302cd7bb96bbf7b9ee9ec0c6e4ef92
unknown <3203317@qq.com>
2015-11-16 16:21:41 +0800

    haha

---
+++ vnc/com/nwyun/birdegg/viewer/Window.java
@@ -0,0 +1,20 @@
+package com.nwyun.birdegg.viewer;
+
+/**
+ * 
+ * @author huangxin (3203317@qq.com)
+ * 
+ */
+public abstract class Window {
+	abstract void setTitle();
+
+	abstract String getTitle();
+
+	abstract void setHeight();
+
+	abstract int getHeight();
+
+	abstract void setWidth();
+
+	abstract int getWidth();
+}
